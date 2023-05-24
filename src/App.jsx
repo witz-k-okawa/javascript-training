09ca11a63393fe3284e6a4ecbcecd392fe2f7ce0
witz-k-okawa <121995444+witz-k-okawa@users.noreply.github.com>
2023-05-24 17:03:34 +0900

    講座完了

--- src/App.jsx
+++ src/App.jsx
@@ -53,7 +53,14 @@ const App = () => {
         todoText={todoText}
         onChange={onChangeTodoText}
         onClick={onClickAdd}
+        disabled={incompleteTodos.length >= 5}
       />
+      {incompleteTodos.length >= 5 && (
+        <p style={{ color: "red" }}>
+          登録できるTODOは5個までです。消化してください。
+        </p>
+      )}
+
       <IncompleteTodoList
         todos={incompleteTodos}
         onClickComplete={onClickComplete}
